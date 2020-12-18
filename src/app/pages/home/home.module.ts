a298e11907a041ceecd4af0528fc27f4ed0bb924
Erikstellet <erikstellet@gmail.com>
2020-12-18 00:20:41 -0300

    Parte 1

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePage } from './home.page';
+import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { homeReducer } from './state/home.reduce';
+
+@NgModule
+({
+  imports:
+  [
+    CommonModule,
+    ReactiveFormsModule,
+    StoreModule.forFeature('home', homeReducer),
+  ],
+  declarations:
+  [
+    HomePage
+  ]
+})
+
+export class HomeModule { }
